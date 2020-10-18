4c2d360b0708bc55b430c9b6da75661fdddab0fb
joansaj <joannsaj@gmail.com>
2020-10-18 09:41:02 +0300

    making api requests

--- app/request.py
+++ app/request.py
@@ -1,4 +1,45 @@
 from app import app
+import urllib.request,json
+from .models import source
 
-# Getting api key
+Source = source.Source
+
 api_key = app.config['NEWS_API_KEY']
+source_url = app.config["NEWS_SOURCE_API"]
+
+def get_sources():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = source_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
+def process_results(source_list):
+    
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        # if poster:
+        #     source_object = Source(id,name,description,url,category,language,country)
+        #     source_results.append(source_object)
+
+    return source_results

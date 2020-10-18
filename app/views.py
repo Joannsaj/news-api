5fb9310737d5bd0644b17828da2425420fc09987
joansaj <joannsaj@gmail.com>
2020-10-18 09:15:00 +0300

    add api key and hide it

--- app/views.py
+++ app/views.py
@@ -3,3 +3,13 @@ from app import app
 
     # message = 'Hello World'
     # return render_template('index.html',massage = message)
+
+@app.route('/')
+def index():
+
+    '''
+    function that returns the index page and its data
+    '''
+
+    title = 'Home - Getting you upto date!'
+    return render_template('index.html', title = title)
